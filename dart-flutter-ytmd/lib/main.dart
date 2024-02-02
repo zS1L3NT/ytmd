c4bdc0cb3f424c6062d7c987de9de85a9d702eb8
zS1L3NT Mac <zechariahtan144@gmail.com>
2024-02-03 05:58:24 +0800

    ✨ add simple js api usage

--- dart-flutter-ytmd/lib/main.dart
+++ dart-flutter-ytmd/lib/main.dart
@@ -1,6 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:ytmd/app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final documents = await getApplicationDocumentsDirectory();
+
+  if (await File("${documents.path}/iframe.js").exists()) {
+    await File("${documents.path}/iframe.js").delete();
+  }
+
+  if (await File("${documents.path}/player.js").exists()) {
+    await File("${documents.path}/player.js").delete();
+  }
+
+  final bytes = await rootBundle.load("bin/ytmd").then((b) => b.buffer.asUint8List());
+  final file = File("${documents.path}/ytmd");
+  await file.writeAsBytes(bytes);
+  await Process.run("chmod", ["755", file.path]);
+
+  final process1 = await Process.run(
+    file.path,
+    ["api.getInfo('v7bnOxV4jAc').then(i => i.basic_info.title)"],
+  );
+  print("stdout1: " + process1.stdout);
+  print("stderr1: " + process1.stderr);
+
+  final process2 = await Process.run(
+    file.path,
+    ["api.getInfo('v7bnOxV4jAc').then(i => i.basic_info.title)"],
+  );
+  print("stdout2: " + process2.stdout);
+  print("stderr2: " + process2.stderr);
+
   runApp(const App());
 }
--- ts-bun-ytmd/src/app.ts
+++ ts-bun-ytmd/src/app.ts
@@ -1,1 +1,37 @@
 import { Innertube } from "youtubei.js"
+
+const directory = process.execPath.replace(/\/ytmd$/, "")
+const iframefile = Bun.file(`${directory}/iframe.js`)
+const playerfile = Bun.file(`${directory}/player.js`)
+
+console.time("YouTube Music API: Initialization")
+let init = true
+let iframejs = (await iframefile.exists()) ? await iframefile.text() : ""
+let playerjs = (await playerfile.exists()) ? await playerfile.text() : ""
+const api = await Innertube.create({
+	fetch: async (url, options) => {
+		if (!init) return fetch(url, options)
+		if (!(url instanceof URL)) throw new Error("URL must be an instance of the URL class")
+		if (url.pathname == "/iframe_api") {
+			if (!iframejs) {
+				iframejs = await fetch(url).then(res => res.text())
+				await Bun.write(iframefile, iframejs)
+			}
+			return new Response(iframejs)
+		} else if (url.pathname.startsWith("/s/player/")) {
+			if (!playerjs) {
+				playerjs = await fetch(url, options).then(res => res.text())
+				await Bun.write(playerfile, playerjs)
+			}
+			return new Response(playerjs)
+		} else {
+			throw new Error(`Unknown URL path: ${url.pathname}`)
+		}
+	},
+})
+init = false
+console.timeEnd("YouTube Music API: Initialization")
+
+console.time(`YouTube Music API: ${Bun.argv[2]}`)
+console.log(JSON.stringify(await eval(Bun.argv[2] || "")))
+console.timeEnd(`YouTube Music API: ${Bun.argv[2]}`)
